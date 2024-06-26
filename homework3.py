82f24f0a0d9a600090df7712984d9a309fb6eb08
R19G <guseynov_rustam2017@mail.ru>
2024-06-26 16:33:08 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,9 @@
+name = "Rustam"
+print(name)
+age = 24
+print(age)
+new_age = (age + 1)
+print(new_age)
+is_student = True
+print(is_student)
+print("Name:", name, "\nAge:", age, "\nNew age:", new_age, "\nIs Student:", is_student)
